f61a51b897bc26ac764288dd19987b9badbd15f5
Youri <youribontekoe@gmail.com>
2021-04-18 21:07:26 +0200

    Updated Migrations

---
+++ Infrastructure.EF/Migrations/20210418190705_v2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.EF.Migrations
+{
+    public partial class v2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "User");
+        }
+    }
+}
